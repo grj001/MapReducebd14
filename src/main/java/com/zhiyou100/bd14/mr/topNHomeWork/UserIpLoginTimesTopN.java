de4696911d0a68624208ecceaef99bf5a2720e7e
guorenjie911 <875654381@qq.com>
2017-10-18 16:27:49 +0800

    1

--- src/main/java/com/zhiyou100/bd14/mr/topNHomeWork/UserIpLoginTimesTopN.java
+++ src/main/java/com/zhiyou100/bd14/mr/topNHomeWork/UserIpLoginTimesTopN.java
@@ -1,5 +1,7 @@
 package com.zhiyou100.bd14.mr.topNHomeWork;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Set;
 import java.util.TreeMap;
@@ -61,7 +63,49 @@ public class UserIpLoginTimesTopN {
 		public int getPartition(Text key, IntWritable value, int numPartitions) {
 
 			String userName = key.toString().split("\\s")[0];
+			
+			
+			
+			
+			
+			
+			
+			try {
+				
+				FileWriter fileWriter = 
+						new FileWriter(
+								new File("/partition.txt")
+								);
+				
+				fileWriter.write("按照userName分区" + userName+"\n");
+				fileWriter.flush();
+				fileWriter.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			System.out.println("按照userName分区" + userName);
 
 			return (userName.hashCode() & Integer.MAX_VALUE) % numPartitions;
